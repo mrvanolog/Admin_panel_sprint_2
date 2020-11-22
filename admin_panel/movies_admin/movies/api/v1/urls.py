c00555e516f56eaf8e3f7e9d10c3c4580d8ccd35
mrvanolog <mrvanolog97@gmail.com>
2020-11-22 17:56:24 +0300

    added class view templates

--- admin_panel/movies_admin/movies/api/v1/urls.py
+++ admin_panel/movies_admin/movies/api/v1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movies/', views.MoviesListApi.as_view())
+    path("movies/", views.Movies.as_view()),
+    path("movies/<uuid:pk>/", views.MoviesDetail.as_view())
 ]
--- admin_panel/movies_admin/movies/api/v1/views.py
+++ admin_panel/movies_admin/movies/api/v1/views.py
@@ -1,9 +1,32 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic.list import BaseListView
+from django.views.generic.detail import BaseDetailView
 
-class MoviesListApi(View):
+from movies_admin.movies.models import FilmWork
+
+
+class MoviesApiMixin:
+    model = FilmWork
     http_method_names = ['get']
 
-    def get(self, request, *args, **kwargs):
-        # Получение и обработка данных
-        return JsonResponse({})
+    def get_queryset(self):
+        return # Сформированный QuerySet
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class Movies(MoviesApiMixin, BaseListView):
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {
+            "results": list(self.get_queryset()),
+        }
+
+
+class MoviesDetail(MoviesApiMixin, BaseDetailView):
+
+    def get_context_data(self, **kwargs):
+        return  # Словарь с данными объекта
