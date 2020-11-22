4d038ed74a72b123032def8f27b740c8cf096836
mrvanolog <mrvanolog97@gmail.com>
2020-11-22 16:20:46 +0300

    deleted logging

--- admin_panel/movies_admin/config/settings/base.py
+++ admin_panel/movies_admin/config/settings/base.py
@@ -130,32 +130,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': True,
-#     'filters': {
-#         'require_debug_true': {
-#             '()': 'django.utils.log.RequireDebugTrue',
-#         }
-#     },
-#     'formatters': {
-#         'default': {
-#             'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
-#         },
-#     },
-#     'handlers': {
-#         'debug-console': {
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'default',
-#             'filters': ['require_debug_true'],
-#         },
-#     },
-#     'loggers': {
-#         'django.db.backends': {
-#             'level': 'DEBUG',
-#             'handlers': ['debug-console'],
-#             'propagate': False,
-#         }
-#     },
-# }
