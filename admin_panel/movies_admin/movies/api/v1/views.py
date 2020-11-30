bd98528ec15ba8111163de8133367312ee7660c6
mrvanolog <mrvanolog97@gmail.com>
2020-11-30 19:28:13 +0300

    added method to upload all movies data

--- admin_panel/movies_admin/movies/api/v1/views.py
+++ admin_panel/movies_admin/movies/api/v1/views.py
@@ -4,29 +4,47 @@ from django.http import JsonResponse
 from django.views.generic.list import BaseListView
 from django.views.generic.detail import BaseDetailView
 
-from movies_admin.movies.models import FilmWork
+from movies.models import FilmWork
 
 
-class MoviesApiMixin:
+class Movies(BaseListView):
     model = FilmWork
-    http_method_names = ['get']
+    http_method_names = ['get']  # Список методов, которые реализует обработчик
 
     def get_queryset(self):
-        return # Сформированный QuerySet
+        values = ("id", "title", "description", "creation_date", "rating", "type")
+        query = FilmWork.objects.values(*values).annotate(
+            actors=ArrayAgg(
+                "personfilmwork__person__full_name",
+                filter=Q(personfilmwork__role="actor"),
+                distinct=True
+            ),
+            directors=ArrayAgg(
+                "personfilmwork__person__full_name",
+                filter=Q(personfilmwork__role="director"),
+                distinct=True
+            ),
+            writers=ArrayAgg(
+                "personfilmwork__person__full_name",
+                filter=Q(personfilmwork__role="writer"),
+                distinct=True
+            ),
+            genres=ArrayAgg(
+                "genres__name",
+                distinct=True
+            ),
+        )
+        return query
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {
+            'results': list(self.get_queryset()),
+        }
+
+        return context
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
 
-
-class Movies(MoviesApiMixin, BaseListView):
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = {
-            "results": list(self.get_queryset()),
-        }
-
-
-class MoviesDetail(MoviesApiMixin, BaseDetailView):
-
-    def get_context_data(self, **kwargs):
-        return  # Словарь с данными объекта
+class MoviesDetail(BaseDetailView):
+    pass
