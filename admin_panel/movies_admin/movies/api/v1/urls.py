78329823e4a933294bb4132c1428ad7bc6d0e1d7
mrvanolog <mrvanolog97@gmail.com>
2020-11-30 19:58:51 +0300

    updated views names

--- admin_panel/movies_admin/movies/api/v1/urls.py
+++ admin_panel/movies_admin/movies/api/v1/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("movies/", views.Movies.as_view()),
-    path("movies/<uuid:pk>/", views.MoviesDetail.as_view())
+    path("movies/", views.MoviesApi.as_view()),
+    path("movies/<uuid:pk>/", views.MoviesDetailApi.as_view())
 ]
